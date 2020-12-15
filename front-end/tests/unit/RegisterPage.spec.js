d758d56240dd9358f78512bd2afd3d4eb34bde9f
YoonSung <opqudk@gmail.com>
2020-12-15 12:39:45 +0900

    vue-test-utils API를 사용한 unit test로 변경

--- front-end/tests/unit/RegisterPage.spec.js
+++ front-end/tests/unit/RegisterPage.spec.js
@@ -1,11 +1,31 @@
-import Vue from 'vue'
+import { mount } from '@vue/test-utils'
 import RegisterPage from '@/views/RegisterPage'
 
 describe('RegisterPage.vue', () => {
-    it('should render correct contents', () => {
-        const Constructor = Vue.extend(RegisterPage)
-        const vm = new Constructor().$mount()
+    let wrapper
+    let fieldUsername
+    let fieldEmailAddress
+    let fieldPassword
+    let buttonSubmit
 
+    beforeEach(() => {
+        wrapper = mount(RegisterPage)
+        fieldUsername = wrapper.find("#username")
+        fieldEmailAddress = wrapper.find("#emailAddress")
+        fieldPassword = wrapper.find("#password")
+        buttonSubmit = wrapper.find('form button[type="submit"]')
+    })
+
+    it('should render correct contents', () => {
+        expect(wrapper.find('.logo').attributes().src).toEqual('/static/images/logo.png')
+        expect(wrapper.find('.tagline').text()).toEqual('Open source task management tool')
+        expect(fieldUsername.element.value).toEqual('')
+        expect(fieldEmailAddress.element.value).toEqual('')
+        expect(fieldPassword.element.value).toEqual('')
+        expect(buttonSubmit.text()).toEqual('Create account')
+
+/*        const Constructor = Vue.extend(RegisterPage)
+        const vm = new Constructor().$mount()
         expect(vm.$el.querySelector('.logo').getAttribute('src'))
             .toEqual('/static/images/logo.png')
         expect(vm.$el.querySelector('.tagline').textContent)
@@ -14,6 +34,6 @@ describe('RegisterPage.vue', () => {
         expect(vm.$el.querySelector('#emailAddress').value).toEqual('')
         expect(vm.$el.querySelector('#password').value).toEqual('')
         expect(vm.$el.querySelector('form button[type="submit"]').textContent)
-            .toEqual('Create account')
+            .toEqual('Create account')*/
     })
 })
